dacb5e6172997163a84616ce6acb06736174146c
Lupang <goldball1012@gmail.com>
2023-07-03 11:57:55 +0900

    0703

--- Source/AIClassModule/AIClassModule.Build.cs
+++ Source/AIClassModule/AIClassModule.Build.cs
@@ -6,6 +6,6 @@ public class AIClassModule : ModuleRules
     {
         PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
-        PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "BaseClassModule", "Niagara" });
+        PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "BaseClassModule", "Niagara" , "InputCore", "UMG", "AIModule" });
     }
 }
