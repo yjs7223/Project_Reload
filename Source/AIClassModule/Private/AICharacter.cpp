42e12ed85f06980a7dd10f47dc8783249a7a8c36
hyobeomJ <107463484+hyobeomJ@users.noreply.github.com>
2023-07-05 16:54:38 +0900

    v0.1.2.10
    Suppression 간접 피격 캡슐

--- Source/AIClassModule/Private/AICharacter.cpp
+++ Source/AIClassModule/Private/AICharacter.cpp
@@ -3,7 +3,9 @@
 
 #include "AICharacter.h"
 #include "AIWeaponComponent.h"
+#include "Components/CapsuleComponent.h"
 #include "AICharacterMoveComponent.h"
+#include "ST_Range.h"
 
 AAICharacter::AAICharacter()
 {
@@ -21,6 +23,17 @@ AAICharacter::AAICharacter()
 	FName WeaponSocket(TEXT("hand_r_Socket"));
 	AIWeapon->WeaponMesh->SetupAttachment(GetMesh(), WeaponSocket);
 
+	static ConstructorHelpers::FObjectFinder<UDataTable> DT_RangeDataObject(TEXT("DataTable'/Game/Aws/AI_Stat/DT_Range.DT_Range'"));
+	if (DT_RangeDataObject.Succeeded())
+	{
+		UE_LOG(LogTemp, Warning, TEXT("DataTable Succeed!"));
+		DT_Range = DT_RangeDataObject.Object;
+	}
+	SetDataTable("Rifle_E");
+
+	CollisionMesh = CreateDefaultSubobject<UCapsuleComponent>(FName("CapSule"));
+	CollisionMesh->SetCapsuleRadius(HitRadius);
+	CollisionMesh->SetCapsuleHalfHeight(HitHeight);
 }
 
 void AAICharacter::BeginPlay()
@@ -31,4 +44,17 @@ void AAICharacter::BeginPlay()
 void AAICharacter::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
+}
+
+void AAICharacter::SetDataTable(FName EnemyName)
+{
+	FST_Range* RangeData = DT_Range->FindRow<FST_Range>(EnemyName, FString(""));
+	if (RangeData)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("EnemyData Succeed!"));
+
+		HitRadius = RangeData->Sup_HitRadius;
+		HitHeight = RangeData->Sup_HitHeight;
+		
+	}
 }
