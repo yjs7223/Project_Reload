5e3f3d1eb253f6b0b590a4c0fd79681cb71f6cb6
sero <48919904+yjs7223@users.noreply.github.com>
2023-07-08 17:06:11 +0900

    v0.1.4.1
    add CollisionChannel

---
+++ Source/PlayerModule/Private/SCrossHairWidget.cpp
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "SCrossHairWidget.h"
+#include "SlateOptMacros.h"
+
+BEGIN_SLATE_FUNCTION_BUILD_OPTIMIZATION
+void SCrossHairWidget::Construct(const FArguments& InArgs)
+{
+	/*
+	ChildSlot
+	[
+		// Populate the widget
+	];
+	*/
+}
+END_SLATE_FUNCTION_BUILD_OPTIMIZATION
