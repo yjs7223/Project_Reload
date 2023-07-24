c47a7eff7239074a4ad4ffbfb1220bf92f743386
Lupang <goldball1012@gmail.com>
2023-07-24 18:39:48 +0900

    v0.1.7.2
    amin update

---
+++ Source/AIClassModule/Public/BTS_Amim.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BehaviorTree/BTService.h"
+#include "BTS_Amim.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AICLASSMODULE_API UBTS_Amim : public UBTService
+{
+	GENERATED_BODY()
+	
+public:
+
+	//virtual void ReceiveActivationAI() override;
+};
