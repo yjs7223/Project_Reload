eebe99449a056c64bc0731aa10fab11d6e58fee1
NoSmile51 <114338041+NoSmile51@users.noreply.github.com>
2023-07-31 20:35:23 +0900

    Update BTT_OrderWaitChk.cpp

--- Source/AIClassModule/Private/BTT_OrderWaitChk.cpp
+++ Source/AIClassModule/Private/BTT_OrderWaitChk.cpp
@@ -20,31 +20,28 @@ EBTNodeResult::Type UBTT_OrderWaitChk::ExecuteTask(UBehaviorTreeComponent& Owner
 	{
 		commander = Cast<AAICommander>(OwnerComp.GetAIOwner());
 	}
-	orderchk = false;
-	for (auto ai : commander->List_Division)
+	if (commander && commander->GetBlackboardComponent())
 	{
-		AIController = nullptr;
-		AIController = Cast<AAI_Controller>(Cast<AAICharacter>(ai.Key)->GetController());
-		if (AIController)
+		orderchk = false;
+		for (auto ai : commander->List_Division)
 		{
-			if (AIController->GetBlackboardComponent())
+			AIController = nullptr;
+			AIController = Cast<AAI_Controller>(Cast<AAICharacter>(ai.Key)->GetController());
+			if (AIController)
 			{
-				if (AIController->GetBlackboardComponent()->GetValueAsBool("OrderWait"))
+				if (AIController->GetBlackboardComponent())
 				{
-					orderchk = true;
+					if (AIController->GetBlackboardComponent()->GetValueAsBool("OrderWait"))
+					{
+						orderchk = true;
+					}
 				}
 			}
 		}
-	}
-	if (commander && commander->GetBlackboardComponent())
-	{
-		if (orderchk)
+		
+		if(!orderchk)
 		{
-			commander->GetBlackboardComponent()->SetValueAsBool("Cmd_OrderWait", true);
-		}
-		else
-		{
-			commander->GetBlackboardComponent()->SetValueAsBool("Cmd_OrderWait", false);
+			commander->GetBlackboardComponent()->SetValueAsBool("CmdAI_OrderWait", false);
 		}
 	}
 	
