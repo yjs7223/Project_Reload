f7d79ddb6a5a56f3cd878c9c69605ec72feb4767
sero <48919904+yjs7223@users.noreply.github.com>
2023-08-01 15:34:30 +0900

    v0.1.9.2

--- Source/AIClassModule/Private/AI_HP_Widget.cpp
+++ Source/AIClassModule/Private/AI_HP_Widget.cpp
@@ -2,4 +2,18 @@
 
 
 #include "AI_HP_Widget.h"
+#include "Animation/WidgetAnimation.h"
+#include "AICharacter.h"
+#include "AIStatComponent.h"
+#include "UMG.h"
 
+
+void UAI_HP_Widget::NativeConstruct()
+{
+
+}
+
+void UAI_HP_Widget::NativeTick(const FGeometry& MyGeometry, float InDeltaTime)
+{
+
+}
--- Source/AIClassModule/Public/AI_HP_Widget.h
+++ Source/AIClassModule/Public/AI_HP_Widget.h
@@ -15,5 +15,36 @@ class AICLASSMODULE_API UAI_HP_Widget : public UUserWidget
 	GENERATED_BODY()
 	
 public:
+	virtual void NativeConstruct() override;
 
+	virtual void NativeTick(const FGeometry& MyGeometry, float InDeltaTime) override;
+
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+		class UAIStatComponent* stat;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (BindWidgetOptional))
+		class UOverlay* HP_Overlay_s;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (BindWidgetOptional))
+		class UImage* HP_Image_s1;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (BindWidgetOptional))
+		class UImage* HP_Image_s2;
+
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (BindWidgetOptional))
+		class UOverlay* HP_Overlay_l;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (BindWidgetOptional))
+		class UImage* HP_Image_l;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (BindWidgetOptional))
+		class UImage* HP_Image_Back;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (BindWidgetOptional))
+		class UProgressBar* HP_Bar;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (BindWidgetOptional))
+		class UProgressBar* HP_BackBar;
 };
