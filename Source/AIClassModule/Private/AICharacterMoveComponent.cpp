a57da112d48b0a4df52d428f84d3ce85659098ea
hyobeomJ <hyobeom5448@gmail.com>
2023-08-10 10:35:31 +0900

    v0.1.10.9
     무브먼트 버그 픽스

--- Source/AIClassModule/Private/AICharacterMoveComponent.cpp
+++ Source/AIClassModule/Private/AICharacterMoveComponent.cpp
@@ -59,7 +59,7 @@ void UAICharacterMoveComponent::TickComponent(float DeltaTime, ELevelTick TickTy
 		{
 			timeDeltaTime = m_ChangeTime;
 		}
-		lerpDeltaTime = timeDeltaTime * 0.2;
+		lerpDeltaTime = timeDeltaTime * 0.5;
 		Move_Speed = FMath::Lerp(500, m_SpdNomal, lerpDeltaTime);
 		aicharacter->GetCharacterMovement()->MaxWalkSpeed = Move_Speed;
 		break;
