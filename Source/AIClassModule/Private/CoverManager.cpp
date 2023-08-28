70b987172a0d21e49c2757b56074f8e2410b074c
hyobeomJ <107463484+hyobeomJ@users.noreply.github.com>
2023-08-28 13:28:34 +0900

    v0.1.12.7

--- Source/AIClassModule/Private/CoverManager.cpp
+++ Source/AIClassModule/Private/CoverManager.cpp
@@ -5,6 +5,8 @@
 #include "CoverSystem.h"
 #include "AICommander.h"
 #include "SubEncounterSpace.h"
+#include "Kismet/KismetMathLibrary.h"
+#include "Math/BoxSphereBounds.h"
 #include "Runtime/Engine/Classes/Kismet/GameplayStatics.h"
 
 ACoverManager::ACoverManager()
@@ -48,5 +50,16 @@ void ACoverManager::ChangeCoverSystem()
 
 	//CoverSystem->GenerateCovers(true, false);
 	CoverSystem->custom_GenerateCovers(true, false, true,2000);
-		
+	TArray<FCoverHandle> testarray;
+	CoverSystem->GetCoversWithinBounds(FBoxSphereBounds(FVector3d(CoverSystem->GetActorLocation()), FVector3d(10000.0f, 10000.0f, 10000.0f), 10000.0f), testarray);
+	if (!testarray.IsEmpty())
+	{
+		commander->CoverArray.Reset();
+		for (auto item : testarray)
+		{
+			FCoverData coverdata;
+			CoverSystem->GetCoverData(item, coverdata);
+			commander->CoverArray.Add(coverdata.Location);
+		}
+	}
 }
