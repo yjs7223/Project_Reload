4522e43e1c0002554e2cba0088c354cc79cff0d3
sero <48919904+yjs7223@users.noreply.github.com>
2023-09-11 21:35:44 +0900

    v1.2.1.4

--- Source/PlayerModule/Private/CompassWidget.cpp
+++ Source/PlayerModule/Private/CompassWidget.cpp
@@ -5,6 +5,7 @@
 #include "Blueprint/WidgetLayoutLibrary.h"
 #include "Kismet/KismetMathLibrary.h"
 #include "PlayerStatComponent.h"
+#include "Camera/CameraComponent.h"
 #include "UMG.h"
 
 void UCompassWidget::NativeConstruct()
@@ -13,6 +14,7 @@ void UCompassWidget::NativeConstruct()
 
 
 	Points_Slot = Cast<UCanvasPanelSlot>(Points_Image->Slot);
+	Goal_Slot = Cast<UCanvasPanelSlot>(Goal_Image->Slot);
 }
 
 void UCompassWidget::NativeTick(const FGeometry& MyGeometry, float InDeltaTime)
@@ -20,23 +22,28 @@ void UCompassWidget::NativeTick(const FGeometry& MyGeometry, float InDeltaTime)
 	Super::NativeTick(MyGeometry, InDeltaTime);
 
 
-	Points_Slot->SetPosition(CalcPointsPos());
-	Goal_Image->SetRenderTranslation(CalcGoalPos());
-	//Goal_Slot->SetPosition(CalcGoalPos());
+	CalcPointsPos();
+	CalcGoalPos();
 }
 
-FVector2D UCompassWidget::CalcPointsPos()
+void UCompassWidget::CalcPointsPos()
 {
 	FVector2D pos;
 	pos.Y = 0;
 	pos.X = (GetOwningPlayer()->GetControlRotation().Yaw - 180) * -1.f * 10.f;
+<<<<<<< Updated upstream
 	return pos;
+=======
+	
+	Points_Slot->SetPosition(pos);
+>>>>>>> Stashed changes
 }
 
-FVector2D UCompassWidget::CalcGoalPos()
+void UCompassWidget::CalcGoalPos()
 {
 	if (GetOwningPlayerPawn()->FindComponentByClass<UPlayerStatComponent>()->InteractActor)
 	{
+<<<<<<< Updated upstream
 		FVector2D pos;
 
 		FVector2D normal;
@@ -53,6 +60,42 @@ FVector2D UCompassWidget::CalcGoalPos()
 		pos.Y = 0;
 		pos.X = val;
 		return pos;
+=======
+		FVector2D pos = FVector2D::ZeroVector;
+		UCameraComponent* camera = GetOwningPlayerPawn()->FindComponentByClass<UCameraComponent>();
+		if (camera)
+		{
+			FVector2D normal;
+			FVector goalLoc = GetOwningPlayerPawn()->FindComponentByClass<UPlayerStatComponent>()->InteractActor->GetActorLocation();
+			FVector cameraLoc = camera->GetComponentLocation();
+			FRotator rot = UKismetMathLibrary::FindLookAtRotation(goalLoc, cameraLoc);
+			if (CheckIfBehind(camera->GetForwardVector(), rot.Vector()))
+			{
+				return;
+			}
+			normal = FVector2D(rot.Vector());
+			normal.Normalize();
+			float fdot = FVector2D::DotProduct(FVector2D(camera->GetForwardVector()), normal);
+			float rdot = FVector2D::DotProduct(FVector2D(camera->GetRightVector()), normal);
+			float val = (rdot / fdot) * 540.f;
+			pos.Y = -75; 
+			pos.X = val;
+			Goal_Slot->SetPosition(pos);
+		}
 	}
-	return FVector2D::ZeroVector;
+}
+
+bool UCompassWidget::CheckIfBehind(FVector cameraForward, FVector lookatNormal)
+{
+	float val = FVector::DotProduct(lookatNormal, cameraForward);
+	float c = UKismetMathLibrary::DegAcos(val);
+	if (c < 120.0f)
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+>>>>>>> Stashed changes
+	}
 }
--- Source/PlayerModule/Private/Crosshair_Widget.cpp
+++ Source/PlayerModule/Private/Crosshair_Widget.cpp
@@ -236,28 +236,29 @@ void UCrosshair_Widget::CheckDie()
 			}
 
 		}
-	}
 
-	UStatComponent* stat = weapon->m_result.GetActor()->FindComponentByClass<UStatComponent>();
-	if (stat)
-	{
-		if (stat->bDie)
+
+		UStatComponent* stat = weapon->m_result.GetActor()->FindComponentByClass<UStatComponent>();
+		if (stat)
 		{
-			UGameplayStatics::PlaySoundAtLocation(this, MyCharacter->CharacterSound->Kill_cue, MyCharacter->GetActorLocation());
-			//stat->isDie = false;
-			Kill_Overlay->SetRenderOpacity(1.0f);
-			GetWorld()->GetTimerManager().ClearTimer(KillTimer);
-			if (IsAnimationPlaying(KillAnim))
+			if (stat->bDie)
 			{
-				StopAnimation(KillAnim);
-			}
+				UGameplayStatics::PlaySoundAtLocation(this, MyCharacter->CharacterSound->Kill_cue, MyCharacter->GetActorLocation());
+				//stat->isDie = false;
+				Kill_Overlay->SetRenderOpacity(1.0f);
+				GetWorld()->GetTimerManager().ClearTimer(KillTimer);
+				if (IsAnimationPlaying(KillAnim))
+				{
+					StopAnimation(KillAnim);
+				}
 
-			GetWorld()->GetTimerManager().SetTimer(KillTimer,
-				FTimerDelegate::CreateLambda([&]()
-					{
-						PlayAnimationForward(KillAnim);
-					}
-			), .01f, false);
+				GetWorld()->GetTimerManager().SetTimer(KillTimer,
+					FTimerDelegate::CreateLambda([&]()
+						{
+							PlayAnimationForward(KillAnim);
+						}
+				), .01f, false);
+			}
 		}
 	}
 }
