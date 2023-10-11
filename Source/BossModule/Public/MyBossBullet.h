3fe249d6a9d0cd5d32454fdbf95453978fb89553
sero <48919904+yjs7223@users.noreply.github.com>
2023-10-11 15:39:27 +0900

    v1.5.6.9
    오류 수정 및 보스 bullet 추가

---
+++ Source/BossModule/Public/MyBossBullet.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Bullet.h"
+#include "MyBossBullet.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BOSSMODULE_API AMyBossBullet : public ABullet
+{
+	GENERATED_BODY()
+	
+};
